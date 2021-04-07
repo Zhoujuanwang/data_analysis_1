88eabba78ec159af8d5a191f96d430b563c296cd
Zhoujuanwang <1810306204@pku.edu.cn>
2021-04-08 07:54:22 +0800

    connections

--- Reading_and_writing_datas.R
+++ Reading_and_writing_datas.R
@@ -27,4 +27,21 @@ serialize()
 args(read.table)
 str(read.table)
 
+# dput() and dget()
+x <- c(1, 2, 3, 5, 8, 10)
+Author <- c("meta", "cox", "inverse", "simultaneous", "cosmopolitan", "sovereign")
+SD <- c(12.3, 4.5, 6.7, 8.9, 12.01, 5.6)
+mydata <- data.frame(x, Author, SD)
+dput(mydata)
+dput(mydata, file = "dupt_try.R")
+newdata <- dget("dupt_try.R")
+newdata
+
+# multiple objects can be deparsed using the dump() function and read 
+#back in using source()
+a <- "foo"
+dump(c("a", "newdata"), file = "dump_try.R")
+rm(a, newdata)
+source("dump_try.R")
+
 
